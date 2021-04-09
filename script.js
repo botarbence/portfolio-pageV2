519e97aa9b7362b5e88d8b325864a91820433b96
botarbence <botarbence@gmail.com>
2021-04-09 10:40:16 +0200

    skill animation

--- script.js
+++ script.js
@@ -17,139 +17,6 @@ const skills = document.querySelector(".skills__container");
 const languageBtns = document.querySelector(".language__buttons");
 const textElements = document.querySelectorAll("[data-key]");
 
-// SHOW MENU
-const showMenu = () => {
-  if ((nav, hamburger)) {
-    hamburger.addEventListener("click", () => {
-      navMenu.classList.toggle("nav__menu--show");
-    });
-  }
-};
-showMenu();
-
-// ACTIVE LINK
-const linkAction = () => {
-  navMenu.classList.remove("nav__menu--show");
-};
-navLinks.forEach((link) => link.addEventListener("click", linkAction));
-
-// SCROLL TO LINK
-const scrollActive = () => {
-  const scrollY = window.pageYOffset;
-  sections.forEach((section) => {
-    if (!section.hasAttribute("id")) return;
-    const sectionHeight = section.offsetHeight;
-    const sectionTop = section.offsetTop - 50;
-
-    sectionId = section.getAttribute("id");
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(`.nav__menu a[href*=${sectionId}]`)
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(`.nav__menu a[href*=${sectionId}]`)
-        .classList.remove("active-link");
-    }
-  });
-};
-window.addEventListener("scroll", scrollActive);
-
-// CHANGE HEADER BACKGROUND
-function scrollHeader() {
-  const sectionTop = about.offsetTop;
-
-  if (this.scrollY >= sectionTop - 50) {
-    header.classList.add("scroll-header");
-  } else {
-    header.classList.remove("scroll-header");
-  }
-}
-window.addEventListener("scroll", scrollHeader);
-
-// SHOW SCROLL TOP
-function scrollToTop() {
-  const sectionTop = about.offsetTop;
-  if (this.scrollY >= sectionTop - 50) {
-    scrollTop.classList.add("show-scroll");
-  } else {
-    scrollTop.classList.remove("show-scroll");
-  }
-}
-window.addEventListener("scroll", scrollToTop);
-
-// DATE
-const date = new Date();
-footerDate.textContent = date.getFullYear();
-
-// HOME PARRALAX
-home.style.backgroundPositionY = 0;
-window.addEventListener("scroll", () => {
-  let scrollY = window.pageYOffset;
-  home.style.backgroundPositionY = `${scrollY * 0.7}px`;
-});
-
-// REVEAL SECTIONS
-const revealSections = (entries, observer) => {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.firstElementChild.classList.remove("hidden");
-  observer.unobserve(entry.target);
-};
-
-const sectionObserver = new IntersectionObserver(revealSections, {
-  root: null,
-  threshold: 0.2,
-});
-
-sections.forEach((section) => {
-  sectionObserver.observe(section);
-});
-
-// DARK MODE
-let screen = true;
-
-screenmode.addEventListener("click", () => {
-  screen = !screen;
-  document.body.classList.toggle("dark-theme");
-  screenmode.innerHTML = screen
-    ? `<span class="sun"><i class="fas fa-sun"></i></span>`
-    : `<span class="sun"><i class="far fa-moon"></i></span>`;
-});
-
-// LOAD PAGE
-
-const init = () => {
-  setTimeout(() => {
-    greeting.classList.remove("greeting__hidden");
-    social.classList.remove("social__hidden");
-    introduction.classList.remove("introduction__hidden");
-  }, 1000);
-};
-
-window.addEventListener("load", init);
-
-// CREATE SKILLS
-
-const createSkills = () => {
-  const markup = `<div class="skills__data">
-  <div class="skills__names">
-    <i class="fab fa-html5 skills__icon"></i>
-    <span class="skills__name">HTML5</span>
-  </div>
-  <div class="skills__bar skills__html">
-  </div>
-  <div>
-    <span class="skills__percentage">90%</span>
-  </div>
-</div>`;
-  skills.innerHTML = markup;
-};
-// createSkills();
-
-// CHANGE LANGUAGE
-
 const data = {
   en: {
     navAbout: "About",
@@ -163,7 +30,7 @@ const data = {
     aboutSubtitle: "Some information",
     aboutTitle: "About Me",
     aboutDescription1:
-      "my name is Bence Botár and I live in Budapest. As a professional musician, my work environment was highly affected by the pandemic and restrictions, so I had to reconsider my future career and possibilities. I’ve always been interested in the creative and challenging tasks, that is why I decided to start learning web development.",
+      "My name is Bence Botár and I live in Budapest. As a professional musician, my work environment was highly affected by the pandemic and restrictions, so I had to reconsider my future career and possibilities. I’ve always been interested in the creative and challenging tasks, that is why I decided to start learning web development.",
     aboutDescription2:
       "In the spring of 2020, I participated in the GreenFox Academy's 'MyFirstApp Coding' bootcamp. After that I successfully completed the 'Redesign Program' supported by the Hungarian state, which aimed to teach basic digital, IT skills and to provide insights about the basics of programming and website building. In parallel I continuously educated myself in the freeCodeCamp and Udemy online web development courses.",
     aboutDescription3:
@@ -257,7 +124,181 @@ const data = {
     footerDescription: "Hope I hear from you soon",
     contactButton: "Küldés",
   },
+  skills: {
+    html: { name: "HTML5", level: 90 },
+    css: { name: "CSS3", level: 80 },
+    javascript: { name: "JavaScript", level: 75 },
+    uxui: { name: "UX/UI", level: 85 },
+    react: { name: "React/Redux", level: 65 },
+  },
 };
+
+// SHOW MENU
+
+const showMenu = () => {
+  if ((nav, hamburger)) {
+    hamburger.addEventListener("click", () => {
+      navMenu.classList.toggle("nav__menu--show");
+    });
+  }
+};
+showMenu();
+
+// ACTIVE LINK
+
+const linkAction = () => {
+  navMenu.classList.remove("nav__menu--show");
+};
+navLinks.forEach((link) => link.addEventListener("click", linkAction));
+
+// SCROLL TO LINK
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+  sections.forEach((section) => {
+    if (!section.hasAttribute("id")) return;
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 50;
+
+    sectionId = section.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`.nav__menu a[href*=${sectionId}]`)
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(`.nav__menu a[href*=${sectionId}]`)
+        .classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+// CHANGE HEADER BACKGROUND
+
+function scrollHeader() {
+  const sectionTop = about.offsetTop;
+
+  if (this.scrollY >= sectionTop - 50) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
+window.addEventListener("scroll", scrollHeader);
+
+// SHOW SCROLL TOP
+
+function scrollToTop() {
+  const sectionTop = about.offsetTop;
+  if (this.scrollY >= sectionTop - 50) {
+    scrollTop.classList.add("show-scroll");
+  } else {
+    scrollTop.classList.remove("show-scroll");
+  }
+}
+window.addEventListener("scroll", scrollToTop);
+
+// DATE
+
+const date = new Date();
+footerDate.textContent = date.getFullYear();
+
+// HOME PARRALAX
+
+home.style.backgroundPositionY = 0;
+window.addEventListener("scroll", () => {
+  let scrollY = window.pageYOffset;
+  home.style.backgroundPositionY = `${scrollY * 0.7}px`;
+});
+
+// REVEAL SECTIONS
+
+const revealSections = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.firstElementChild.classList.remove("hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSections, {
+  root: null,
+  threshold: 0.2,
+});
+
+sections.forEach((section) => {
+  sectionObserver.observe(section);
+});
+
+// DARK MODE
+
+let screen = true;
+
+screenmode.addEventListener("click", () => {
+  screen = !screen;
+  document.body.classList.toggle("dark-theme");
+  screenmode.innerHTML = screen
+    ? `<span class="sun"><i class="fas fa-sun"></i></span>`
+    : `<span class="sun"><i class="far fa-moon"></i></span>`;
+});
+
+// LOAD PAGE
+
+const init = () => {
+  setTimeout(() => {
+    greeting.classList.remove("greeting__hidden");
+    social.classList.remove("social__hidden");
+    introduction.classList.remove("introduction__hidden");
+  }, 1000);
+};
+
+window.addEventListener("load", init);
+
+// CREATE SKILLS
+
+const createSkills = () => {
+  const markup = Object.values(data.skills)
+    .map((skill, i) => {
+      return `<div class="skill__data skill__hidden" style="transition: all 0.4s ${
+        1 + i * 0.2
+      }s" >
+    <div class="skill__names">
+    <i class="fab fa-html5 skill__icon"></i>
+    <span class="skill__name">${skill.name}</span>
+    </div>
+    <div class="skill__bar" style="width:${skill.level}%;">
+    </div>
+    <div>
+    <span class="skill__percentage">${skill.level}%</span>
+    </div>
+    </div>`;
+    })
+    .join("");
+  skills.innerHTML = markup;
+};
+createSkills();
+
+// REVEAL SKILLS
+
+const revealSkills = (entries, observer) => {
+  [entry] = entries;
+  if (!entry.isIntersecting) return;
+  [...entry.target.children].forEach((skill) =>
+    skill.classList.remove("skill__hidden")
+  );
+
+  observer.unobserve(entry.target);
+};
+
+const skillsObserver = new IntersectionObserver(revealSkills, {
+  root: null,
+  threshold: 0.1,
+});
+
+skillsObserver.observe(skills);
+
+// CHANGE LANGUAGE
 
 const setLanguage = (language) => {
   textElements.forEach((element) => {
